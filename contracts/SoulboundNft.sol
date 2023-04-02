93360582b07aa0de60cc5b39f52edadd78ba6526
eugenioclrc <eugenioclrc@gmail.com>
2023-04-02 13:57:21 -0300

    lib path fix

--- contracts/SoulboundNft.sol
+++ contracts/SoulboundNft.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
-import {ERC721} from "../node_modules/solmate/src/tokens/ERC721.sol";
+import {ERC721} from "solmate/src/tokens/ERC721.sol";
 
 abstract contract SoulboundNft is ERC721 {
     function approve(address, uint256) public override {
